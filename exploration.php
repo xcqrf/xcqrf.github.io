e70a2ec1307dc5d27d4d9acf2ecbb5934e09a8ef
Xiaonan Chen <Xiaonan@mwc-011110.dhcp.missouri.edu>
2015-09-16 15:11:42 -0500

    php test

---
+++ exploration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <title>php exploration</title>
+    
+  </head>
+  <body>
+     
+  <?php
+      echo "Today is " . date("Y/m/d") . "<br>";
+      echo "Today is " . date("Y.m.d") . "<br>";
+      echo "Today is " . date("Y-m-d") . "<br>";
+      echo "Today is " . date("l");
+   ?>
+    
+    
+  </body>
+</html>
